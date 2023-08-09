033cdff171f4a01c65803e669809919b21dccd5d
Andres Cardoso <andress.cardosoo@gmail.com>
2023-08-09 16:37:59 -0300

    segundo commit

--- js/login.js
+++ js/login.js
@@ -4,14 +4,34 @@ const boton = document.getElementById('boton_login')
 
 
 
-boton.addEventListener('click', (e) => {
+boton.addEventListener('click', (e)=> {
     e.preventDefault()
     const data = {
         usuario: username.value,
         contrasena: contrasena.value
     }
 
+    if (username.value !== "" && contrasena.value !== ""){
+        location.href = "index.html";
+    } else {
+        showAlertError()
+        
+    }
     console.log(data)
+    
 })
+
+function showAlertSuccess() {
+    document.getElementById("alert-success").classList.add("show");
+}
+
+function showAlertError() {
+    document.getElementById("alert-danger").classList.add("show");
+    setTimeout(function() {
+        document.getElementById("alert-danger").reset;
+        location.reload();
+    }, 2000);
+}
+
 
 
