8dd6792613235b43eaf06fd916f3e2605eb5dbf9
Anabel Casero <anabelcasero19@gmail.com>
2023-08-11 19:35:04 -0300

    modificacion de login

--- js/login.js
+++ js/login.js
@@ -5,7 +5,7 @@ const contrasena = document.getElementById('contrasena')
 const boton_login = document.getElementById('boton_login')
 const boton_registro = document.getElementById('boton_register')
 
-//usuarios y contrasenas genericas, "como si estuvieran en la base de datos"
+/*usuarios y contrasenas genericas, "como si estuvieran en la base de datos"
 const usuarios = {
     usuario1 : "usuario1",
     usuario2 : "usuario2",
@@ -25,11 +25,30 @@ const contrasenas = {
     contrasena6 : "contrasena6",
     contrasena7 : "contrasena7",
 }
-
+*/
 //Codigo anterior para el log in, ahora para el registro. Anaden cualquier cosa y los deja entrar
 boton_registro.addEventListener('click', (e)=> {
     e.preventDefault()
     if (username.value !== "" && contrasena.value !== ""){
+        localStorage.setItem(contrasena.value, username.value)
+        localStorage.setItem('isLoggedIn', 'true')
+        location.href = "index.html";
+    } else {
+        showAlertError()
+        
+    }
+})
+function verif(){
+    if (username === 'usuario' && contrasena === 'contrasena') {
+        window.location.href = 'index.html'; // Redirigir a la página protegida
+    } else {
+        alert('Credenciales incorrectas');
+    }
+};
+boton_login.addEventListener('click', (e)=> {
+    e.preventDefault()
+    if (username.value !== "" && contrasena.value !== ""){
+        localStorage.setItem('isLoggedIn', 'true')
         location.href = "index.html";
     } else {
         showAlertError()
@@ -37,7 +56,7 @@ boton_registro.addEventListener('click', (e)=> {
     }
 })
 
-//Nuevo evento que utiliza los usuarios de la "base de datos" ficticia. Se loguean utilizando usuarios y contrasenas ya existentes
+/*Nuevo evento que utiliza los usuarios de la "base de datos" ficticia. Se loguean utilizando usuarios y contrasenas ya existentes
 boton_login.addEventListener('click', (e)=> {
     e.preventDefault()
     if (username.value !== "" && contrasena.value !== ""){
@@ -50,12 +69,7 @@ boton_login.addEventListener('click', (e)=> {
     } else {
     showAlertError()
 }
-})
-
-function showAlertSuccess() {
-    document.getElementById("alert-success").classList.add("show");
-}
-
+})*/
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
     setTimeout(function() {
