020007fecd00275fbcfa96735465b842bfef4a3d
Fede-357 <fedelarra.07@gmail.com>
2023-08-14 22:12:33 -0300

    Catalogo de autos

--- js/products.js
+++ js/products.js
@@ -9,18 +9,26 @@ function showProducts(products) {
     for (let i = 0; i < products.length; i++) {
         let product = products[i];
         htmlContentToAppend += `
-        <div class="product">
-          <img src="${product.image}" alt="${product.name}" class="product-image">
+        <div class="container list-group">
+        <div class="product row m-4 ">
+        <div class="col-3 list-group-item d-flex justify-content-between">
+          <img src="${product.image}" alt="${product.name}" class="product-image img-thumbnail">
+        </div>
+        <div class="col-7">
           <h2 class="product-name">${product.name}</h2>
           <p class="product-description">${product.description}</p>
           <p class="product-cost">${product.currency} ${product.cost}</p>
+        </div>
+        <div class="col-2 text-muted">
           <p class="product-sold">${product.soldCount} vendidos</p>
         </div>
+        </div>
+        </div>
       `;
-    };
+    }
 
     container.innerHTML = htmlContentToAppend;
-};
+}
 
 
 async function getProducts() {
