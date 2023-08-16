68a48e4df2fab84ebc2552ba56cc2964133da4ab
Andres Cardoso <andress.cardosoo@gmail.com>
2023-08-16 14:26:11 -0300

    5

--- js/products.js
+++ js/products.js
@@ -1,3 +1,8 @@
+const categoria = localStorage.getItem('catID'); // Ya que los archivos index.js y categories.js ya incluyen la función de guardar la id de la
+                                                 // categoría en localStorage, accedemos a ella
+const DATA_URL = "https://japceibal.github.io/emercado-api/cats_products/" + categoria + ".json"; // y reemplazamos en la url por la id de la api correspondiente
+const container = document.getElementById("product-list");
+
 function showProducts(products) {
     let htmlContentToAppend = "";
 
