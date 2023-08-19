58ad8b5c7d9d6a9f704eeee662c831202aaa1f04
miguelsoffarelli <137739802+miguelsoffarelli@users.noreply.github.com>
2023-08-18 22:53:32 -0300

    6 caracteres necesarios y campos requeridos al registrarse

--- js/register.js
+++ js/register.js
@@ -9,7 +9,7 @@ const email = document.getElementById("email");
 
 regbtn.addEventListener('click', (e)=> { //añadimos un manejador de eventos para click
     e.preventDefault()
-    if (fname.value !== "" && lname.value !=="" && usuario.value !== "" && pass1.value !== "" && pass2.value && email.value !== ""){ //si se cumplen estas condiciones 
+    if (fname.value !== "" && lname.value !=="" && usuario.value !== "" && pass1.value !== "" && pass2.value && pass1.value.length >= 6 && email.value !== ""){ //si se cumplen estas condiciones 
         localStorage.setItem("user", usuario.value);                                                                             //se guardan los datos en localStorage
         localStorage.setItem('isLoggedIn', 'true');                                                                        //se guarda la sesión 
         showAlertSuccess();                                                                                                //se muestra alerta de éxito
@@ -17,10 +17,9 @@ regbtn.addEventListener('click', (e)=> { //añadimos un manejador de eventos par
             location.href = "index.html"                                                                                   //y se redirecciona a la página principal
         }, 2000);                                                                                                                                                                                                       
     } else {                                                                                                               //de lo contrario
-        showAlertError()                                                                                                   //se muestra la alerta
-        
-    }
-})
+        showAlertError()                                                                                                   //se muestra la alerta        
+    };
+});
 
 function showAlertError() { //función que muestra la alerta
     document.getElementById("alert-danger").classList.add("show");
@@ -28,11 +27,11 @@ function showAlertError() { //función que muestra la alerta
         document.getElementById("alert-danger").reset;
         location.reload();
     }, 2000);
-}
+};
 
 function showAlertSuccess() { //función que muestra la alerta
     document.getElementById("alert-success").classList.add("show");
     setTimeout(function() {
         document.getElementById("alert-success").reset;
     }, 2000);
-}
+};
