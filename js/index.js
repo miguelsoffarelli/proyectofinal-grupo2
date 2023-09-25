f24c168f4769d879181e4e547a161f69dcb860d7
Andres Cardoso <andress.cardosoo@gmail.com>
2023-09-24 21:42:20 -0300

    Agregamos musica

--- js/index.js
+++ js/index.js
@@ -157,3 +157,5 @@ CURRENCY_DIV.addEventListener("mouseleave", function(event) {
 BTN_MODE.addEventListener("click", ()=> {
   BODY.classList.toggle("darkMode");
 });
+
+
--- js/init.js
+++ js/init.js
@@ -62,3 +62,40 @@ function cutString(string, limit) {
   }; 
 };
 
+// ---------------------------------------------------------- //
+
+const reproductor = document.getElementById("reproductor");
+const btnReproducir = document.getElementById("btnReproducir");
+const btnPausa = document.getElementById("btnPausa");
+let reproduciendoAntes = false; // Variable para almacenar el estado de reproducción
+
+    // Almacenar la posición y estado de reproducción antes de cambiar de página
+    window.onbeforeunload = () => {
+      localStorage.setItem("posicionReproductor", reproductor.currentTime);
+      localStorage.setItem("reproduciendoAntes", reproductor.paused ? "no" : "si");
+    };
+
+    // Restaurar la posición y estado de reproducción al cargar la página
+    window.onload = () => {
+      const posicionGuardada = localStorage.getItem("posicionReproductor");
+      const reproduciendo = localStorage.getItem("reproduciendoAntes");
+      
+      if (posicionGuardada) {
+        reproductor.currentTime = parseFloat(posicionGuardada);
+        reproduciendoAntes = reproduciendo === "si"; // Actualizar el estado de reproducción
+      }
+      
+      if (reproduciendoAntes) {
+        reproductor.play(); // Reanudar la reproducción si estaba en reproducción antes
+      }
+    };
+
+    btnReproducir.addEventListener("click", () => {
+      reproductor.play();
+      reproduciendoAntes = true; // Actualizar el estado de reproducción
+    });
+
+    btnPausa.addEventListener("click", () => {
+      reproductor.pause();
+      reproduciendoAntes = false; // Actualizar el estado de reproducción
+    });
