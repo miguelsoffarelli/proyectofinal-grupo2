0ecdd890c08ba32634f4a34fdfbffc6c05a37978
Perci237 <diazzfranco23@gmail.com>
2023-10-06 22:37:20 -0300

    Cambios al Login

--- js/login.js
+++ js/login.js
@@ -4,8 +4,19 @@ const boton_login = document.getElementById('boton_login');
 const boton_registro = document.getElementById('boton_register');
 const saltarLogin = document.getElementById('skip');
 const rememberMeCheckbox = document.getElementById('rememberMe');
+<<<<<<< Updated upstream
 
 
+=======
+const savedUsers = JSON.parse(localStorage.getItem("usersList"));
+const inputPass = document.querySelector(".inputPass"); // Esto trae al input de contraseña
+const showPassBtn = document.getElementById("showPassBtn"); // el boton de mostrar contraseña
+const iconCont = document.querySelector(".inputIconPassCon"); //Esto trae al div contenedor del icono.
+const iconPass = document.querySelector(".iconPass"); // Esto trae el icono
+const slideableWall = document.querySelector(".slideableWall"); // La pared deslizante.
+const btnRegisterHere = document.getElementById('btnRegisterHere'); //La constante que trae al botón "¡Registrate aqui!"
+const mensaje = document.getElementById("mensaje");
+>>>>>>> Stashed changes
 // Función que muestra error si las credenciales son incorrectas
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
@@ -50,7 +61,9 @@ saltarLogin.addEventListener('click', (e)=> {
     location.href = "index.html";
 });
 
+
 window.addEventListener('load', () => {
+<<<<<<< Updated upstream
     const savedUser = localStorage.getItem('savedUser');
     const savedPassword = localStorage.getItem('savedPassword');
   
@@ -59,8 +72,52 @@ window.addEventListener('load', () => {
       contrasena.value = savedPassword;
     };
   });
+=======
+  const savedUser = localStorage.getItem('savedUser');
+  const savedPassword = localStorage.getItem('savedPassword');
+
+  if (savedUser && savedPassword) {
+    username.value = savedUser;
+    contrasena.value = savedPassword;
+  };
+}); 
+>>>>>>> Stashed changes
   
   boton_registro.addEventListener('click', (e)=> {
     e.preventDefault();
     location.href = "register.html";
+}); /**/
+
+/* Código del botón mostrar contraseña */
+
+showPassBtn.addEventListener("click", (e) => {
+    const cambiarType = inputPass;
+    var icon = iconPass;
+    var iconMsg = iconCont;
+    if(cambiarType.type == "password"){
+        cambiarType.type = "text";
+        icon.classList.remove("fa-eye");
+        icon.classList.add("fa-eye-slash");
+        iconMsg.title = "Ocultar Contraseña";
+    }else {
+        cambiarType.type = "password";
+        icon.classList.remove("fa-eye-slash");
+        icon.classList.add("fa-eye");
+        iconMsg.title = "Mostrar Contraseña";
+    }
 });
+
+/* Codigo de la pared deslizante */
+ 
+btnRegisterHere.addEventListener('click', (e) => {
+    var newMensaje = mensaje
+    slideableWall.classList.toggle("slideLeft");
+    if(newMensaje.textContent === "¿No tienes una cuenta?") {
+
+        newMensaje.textContent = "¿Ya tienes una cuenta? ¡Entonces inicia sesion!"
+    } else {
+        newMensaje.textContent = "¿No tienes una cuenta?";
+    }
+});
+
+/* FALTA QUE CAMBIE EL MENSAJE DEL BOTON DE REGISTRO */
