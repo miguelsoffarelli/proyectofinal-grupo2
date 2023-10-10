22140c34c553e7df2c22aaf7bb7668de0aca42e1
miguelsoffarelli <137739802+miguelsoffarelli@users.noreply.github.com>
2023-10-09 21:28:15 -0300

    animaciones carrito

--- js/cart.js
+++ js/cart.js
@@ -18,7 +18,8 @@ async function showCart(data) {
     CART_CONTENT.forEach(product => {
         const articleIndex = articles.findIndex(article => article.id === product.id);
         if (articleIndex === -1) {
-            htmlContentToAppend += `
+            if(product.currency === 'USD'){
+                htmlContentToAppend += `
                 <div class="container-fluid list-group m-4 producto">
                     <div class="product row list-group-item list-group-item-action d-flex justify-content-between">
                         <div class="col-3">
@@ -26,10 +27,10 @@ async function showCart(data) {
                         </div>
                         <div class="col-7">
                             <h2 class="product-name">${product.name}</h2>
-                            <p class="product-cost" data-name='${product.name}' data-cost='${hasDiscount(product.id, product.cost)}'>${product.currency} ${hasDiscount(product.id, product.cost)} c/u</p>
+                            <p class="product-cost" data-name='${product.name}' data-cost='${hasDiscount(product.id, (product.cost / exchangeRateUsd).toFixed(0))}'>${selectedCur} ${hasDiscount(product.id, (product.cost / exchangeRateUsd).toFixed(0))} c/u</p>
                         </div>
                         <div class="col-1">
-                            <input type="number" class='row contador' id="${product.id}" min="1" value="${product.count}" data-cur='${product.currency}' data-cost='${hasDiscount(product.id, product.cost)}' style="width: 2.5rem"/>
+                            <input type="number" class='row contador' id="${product.id}" min="1" value="${product.count}" data-cur='${product.currency}' data-cost='${hasDiscount(product.id, (product.cost / exchangeRateUsd).toFixed(0))}' style="width: 2.5rem"/>
                         </div>
                         <div class="row">
                             <div class="col-1 offset-11 d-flex justify-content-end align-items-end mb-3">
@@ -39,13 +40,38 @@ async function showCart(data) {
                     </div>
                 </div>
             `;
+            } else if (product.currency === 'UYU') {
+                htmlContentToAppend += `
+                <div class="container-fluid list-group m-4 producto">
+                    <div class="product row list-group-item list-group-item-action d-flex justify-content-between">
+                        <div class="col-3">
+                            <img src="${product.img}" alt="${product.name}" class="product-image img-thumbnail">
+                        </div>
+                        <div class="col-7">
+                            <h2 class="product-name">${product.name}</h2>
+                            <p class="product-cost" data-name='${product.name}' data-cost='${hasDiscount(product.id, (product.cost / exchangeRateUyu).toFixed(0))}'>${selectedCur} ${hasDiscount(product.id, (product.cost / exchangeRateUyu).toFixed(0))} c/u</p>
+                        </div>
+                        <div class="col-1">
+                            <input type="number" class='row contador' id="${product.id}" min="1" value="${product.count}" data-cur='${product.currency}' data-cost='${hasDiscount(product.id, (product.cost / exchangeRateUyu).toFixed(0))}' style="width: 2.5rem"/>
+                        </div>
+                        <div class="row">
+                            <div class="col-1 offset-11 d-flex justify-content-end align-items-end mb-3">
+                                <i class="row bin fa-solid fa-trash fa-xl"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            `;
+            };
+            
         } else {
             articles[articleIndex].count += product.count;
         }
     });
   };
   articles.forEach(article => {
-      htmlContentToAppend += `
+    if (article.currency === 'USD'){
+        htmlContentToAppend += `
           <div class="container list-group m-4 producto">
               <div class="product row list-group-item list-group-item-action d-flex justify-content-between">
                   <div class="col-3">
@@ -53,10 +79,10 @@ async function showCart(data) {
                   </div>
                   <div class="col-7">
                       <h2 class="product-name">${article.name}</h2>
-                      <p class="product-cost" data-name='${article.name}' data-cost='${hasDiscount(article.id, article.unitCost)}'>${article.currency} ${hasDiscount(article.id, article.unitCost)} c/u</p>
+                      <p class="product-cost" data-name='${article.name}' data-cost='${hasDiscount(article.id, (article.unitCost / exchangeRateUsd).toFixed(0))}'>${selectedCur} ${hasDiscount(article.id, (article.unitCost / exchangeRateUsd).toFixed(0))} c/u</p>
                   </div>
                   <div class="col-1">
-                      <input type="number" class='contador row' id="${article.id}" min="1" value="${article.count}" data-cur='${article.currency}' data-cost='${hasDiscount(article.id, article.unitCost)}' style="width: 5vh"/>
+                      <input type="number" class='contador row' id="${article.id}" min="1" value="${article.count}" data-cur='${article.currency}' data-cost='${hasDiscount(article.id, (article.unitCost / exchangeRateUsd).toFixed(0))}' style="width: 5vh"/>
                   </div>
                   <div class="row">
                       <div class="col-1 offset-11 d-flex justify-content-end align-items-end mb-3">
@@ -66,6 +92,30 @@ async function showCart(data) {
               </div>
           </div>
       `;
+    } else if (article.currency === 'UYU'){
+        htmlContentToAppend += `
+          <div class="container list-group m-4 producto">
+              <div class="product row list-group-item list-group-item-action d-flex justify-content-between">
+                  <div class="col-3">
+                      <img src="${article.image}" alt="${article.name}" class="product-image img-thumbnail">
+                  </div>
+                  <div class="col-7">
+                      <h2 class="product-name">${article.name}</h2>
+                      <p class="product-cost" data-name='${article.name}' data-cost='${hasDiscount(article.id, (article.unitCost / exchangeRateUyu).toFixed(0))}'>${selectedCur} ${hasDiscount(article.id, (article.unitCost / exchangeRateUyu).toFixed(0))} c/u</p>
+                  </div>
+                  <div class="col-1">
+                      <input type="number" class='contador row' id="${article.id}" min="1" value="${article.count}" data-cur='${article.currency}' data-cost='${hasDiscount(article.id, (article.unitCost / exchangeRateUyu).toFixed(0))}' style="width: 5vh"/>
+                  </div>
+                  <div class="row">
+                      <div class="col-1 offset-11 d-flex justify-content-end align-items-end mb-3">
+                          <i class="row bin fa-solid fa-trash fa-xl"></i>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      `;
+    };
+      
   });
   htmlContentToAppend += `
     <div class="empty-cart-btn d-flex justify-content-end align-items-center p-5">
@@ -88,9 +138,11 @@ async function showCart(data) {
         
         total += itemQuantity * itemPrice;
         if(itemCur === 'USD'){
+          total += itemQuantity * (itemPrice / exchangeRateUsd);
           totalElement.textContent = `Total: ${selectedCur} ${(total / exchangeRateUsd).toFixed(2)}`;
         } else if(itemCur === 'UYU'){
-          totalElement.textContent = `Total: ${selectedCur} ${(total / exchangeRateUyu).toFixed(2)}`;
+            total += itemQuantity * (itemPrice / exchangeRateUyu);
+            totalElement.textContent = `Total: ${selectedCur} ${(total / exchangeRateUyu).toFixed(2)}`;
         };
     });  
   };
