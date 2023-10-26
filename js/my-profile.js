28fda834bc14f6a6cffeb90886a5a7d0960afb96
Perci237 <diazzfranco23@gmail.com>
2023-10-25 22:21:10 -0300

    Mejoras a Perfil

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,8 +1,13 @@
 const img = document.getElementById("img");
 const profilePicFile = document.getElementById("profilePicFile");
 const profileUserNameCont = document.getElementById("profileUserName");
-
-
+const defaultUserPic = "img/img_perfil.png"
+const userPicVerified = localStorage.getItem("userPicVerified");
+const resetPicBtn = document.getElementById("resetProfilePic");
+const logOut = document.getElementById("logOut");
+const newNameInput = document.getElementById("changeName");
+const nameChangeBtn = document.getElementById("changeNameBtn");
+const inputBox = document.querySelector(".inputBox");
 
 profilePicFile.addEventListener("change", e => {
     
@@ -11,7 +16,8 @@ profilePicFile.addEventListener("change", e => {
         const reader = new FileReader();
         reader.onload = function(e){
             img.src = e.target.result;
-            localStorage.setItem("userPic", img.getAttribute("src"))
+            localStorage.setItem("userPic", img.getAttribute("src"));
+            localStorage.setItem("userPicVerified", true);
         }
         reader.readAsDataURL(e.target.files[0])
     }else{
@@ -19,6 +25,13 @@ profilePicFile.addEventListener("change", e => {
     }
     
 });
+
+resetProfilePic.addEventListener("click", e => {
+
+    img.src = defaultUserPic;
+    localStorage.setItem("userPic", img.getAttribute("src"));
+})
+
 
 function putUserName() {
     let h4 = document.createElement("h4");
@@ -33,3 +46,11 @@ window.addEventListener("load", () => {
     img.setAttribute("src", localStorage.getItem("userPic"));
 
 })
+logOut.addEventListener("click", cerrarSesion);
+
+nameChangeBtn.addEventListener("click", () => {
+
+    document.querySelector("h4").toggleAttribute("hidden");
+    inputBox.toggleAttribute("hidden");
+});
+
